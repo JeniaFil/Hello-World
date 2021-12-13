bb0ed73b52f148e6d5c4d3e74de2e2b563f471d6
Jenia <ka1nqz@gmail.com>
2021-12-13 18:05:57 +0300

    all files added to the project

---
+++ main.py
@@ -0,0 +1,15 @@
+# import random
+# a = tuple(random.randint(0,5)for i in range(0,5))
+# b = tuple(random.randint(-5,0)for i in range(-5,0))
+# c = a + b
+# print (c)
+# print(c.count(0)
+
+
+# a = ('т','т','а','и','и','а','и','и','и','т','т','а','и','и','и','и','и','т','и')
+# count = []
+# for i in a:
+#     if i not in count:
+#         count.append(i)
+# for i in count:
+#     print(f'{i} - {a.count(i)}')
